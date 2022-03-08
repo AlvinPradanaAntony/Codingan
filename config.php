6a7ab946f9a01a715ada95b71e92a5af545ca9ab
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-03-08 09:51:16 +0700

    sd

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost/phpmyadmin';
+$databaseName = 'db_user';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, 
+$databasePassword, $databaseName); 
+?>
