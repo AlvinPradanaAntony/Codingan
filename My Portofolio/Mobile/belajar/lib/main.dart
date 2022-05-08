dbd1bcb7a1bd749f55ae5df31002af2e9ba5b99a
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-05-08 11:54:01 +0700

    Update

--- My Portofolio/Mobile/belajar/lib/main.dart
+++ My Portofolio/Mobile/belajar/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   // runApp(const Myapp());
@@ -108,6 +109,56 @@ void main() {
 //     );
 //   }
 // }
+// final List<String> imgList = [
+//   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+//   'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+//   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+//   'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+//   'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+//   'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+// ];
+
+// final List<Widget> imageSliders = imgList
+//     .map((item) => Container(
+//           child: Container(
+//             margin: const EdgeInsets.all(5.0),
+//             child: ClipRRect(
+//                 borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+//                 child: Stack(
+//                   children: <Widget>[
+//                     Image.network(item, fit: BoxFit.cover, width: 1000.0),
+//                     Positioned(
+//                       bottom: 0.0,
+//                       left: 0.0,
+//                       right: 0.0,
+//                       child: Container(
+//                         decoration: const BoxDecoration(
+//                           gradient: LinearGradient(
+//                             colors: [
+//                               Color.fromARGB(200, 0, 0, 0),
+//                               Color.fromARGB(0, 0, 0, 0)
+//                             ],
+//                             begin: Alignment.bottomCenter,
+//                             end: Alignment.topCenter,
+//                           ),
+//                         ),
+//                         padding: const EdgeInsets.symmetric(
+//                             vertical: 10.0, horizontal: 20.0),
+//                         child: Text(
+//                           'No. ${imgList.indexOf(item)} image',
+//                           style: const TextStyle(
+//                             color: Colors.white,
+//                             fontSize: 20.0,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 )),
+//           ),
+//         ))
+//     .toList();
 
 // ignore: use_key_in_widget_constructors
 class Myapp extends StatelessWidget {
@@ -322,56 +373,149 @@ class Myapp extends StatelessWidget {
   // }
 
 // Export Widget
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Scaffold(
+//           appBar: AppBar(
+//             title: const Text("Image Widget"),
+//             centerTitle: true,
+//           ),
+//           body: ListView.builder(
+//             itemCount: 25,
+//             itemBuilder: (BuildContext context, int index){
+//               return
+//               ListData(
+//                 title: faker.person.name(),
+//                 subtitle: faker.lorem.sentence(),
+//                 imageUrl: 'https://picsum.photos/id/$index/200/300',
+//               );
+//             },
+//           ),
+//         )
+//     );
+//   }
+// }
+
+// class ListData extends StatelessWidget {
+//   final String title;
+//   final String subtitle;
+//   final String imageUrl;
+
+//   // ignore: use_key_in_widget_constructors
+//   const ListData({required this.title, required this.subtitle, required this.imageUrl,});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       contentPadding: const EdgeInsets.all(10),
+//       title: Text(
+//         title,
+//       ),
+//       subtitle: Text(
+//         subtitle,
+//         maxLines: 2,
+//         textAlign: TextAlign.justify,
+//         overflow: TextOverflow.ellipsis,
+//       ),
+//       leading: CircleAvatar(
+//         backgroundImage: NetworkImage(imageUrl),
+//       ),
+//       trailing: const Text("10.00"),
+//       );
+//   }
+
+// Carousel Widget
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Carousel Widget"),
+//           centerTitle: true,
+//         ),
+//         body: Container(
+//         child: CarouselSlider(
+//           options: CarouselOptions(
+//             autoPlay: true,
+//             aspectRatio: 2.0,
+//             enlargeCenterPage: true,
+//           ),
+//           items: imageSliders,
+//         ),
+//       ),
+//       )
+//   );
+// }
+
+// Carousel.builder
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Image Widget"),
-            centerTitle: true,
-          ),
-          body: ListView.builder(
-            itemCount: 25,
-            itemBuilder: (BuildContext context, int index){
-              return 
-              ListData(
-                title: faker.person.name(), 
-                subtitle: faker.lorem.sentence(), 
-                imageUrl: 'https://picsum.photos/id/$index/200/300',
-              );
-            },
-          ),       
-        )
-    );
-  }
-}
-
-class ListData extends StatelessWidget {
-  final String title;
-  final String subtitle;
-  final String imageUrl;
-
-  // ignore: use_key_in_widget_constructors
-  const ListData({required this.title, required this.subtitle, required this.imageUrl,});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.all(10),
-      title: Text(
-        title,
-      ),
-      subtitle: Text(
-        subtitle,
-        maxLines: 2,
-        textAlign: TextAlign.justify,
-        overflow: TextOverflow.ellipsis,
-      ),
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(imageUrl),
-      ),
-      trailing: const Text("10.00"),
-      );
+            appBar: AppBar(
+              title: const Text("Carousel Widget"),
+              centerTitle: true,
+            ),
+            body: CarouselSlider.builder(
+              itemCount: imgList.length,
+              itemBuilder:
+                  (BuildContext context, int itemIndex, int pageViewIndex) =>
+                      Container(
+                margin: const EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Image.network(imgList[itemIndex],
+                            fit: BoxFit.cover, width: 1000.0),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Text(
+                              'No. ${itemIndex+1} image',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+              ),
+            )));
   }
 }
