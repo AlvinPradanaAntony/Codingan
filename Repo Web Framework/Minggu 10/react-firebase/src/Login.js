cafb528761cdcc028d4072905fe8a64b037d7f83
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-05-28 22:45:20 +0700

    Update

--- Repo Web Framework/Minggu 10/react-firebase/src/Login.js
+++ Repo Web Framework/Minggu 10/react-firebase/src/Login.js
@@ -7,6 +7,16 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setErrors] = useState("");
+  const [user, setUser] = useState({});
+
+  firebase.auth().onAuthStateChanged((user) => {
+    setUser(user);
+  });
+
+  const logout = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  };
 
   const Auth = useContext(AuthContext);
   const handleForm = (e) => {
@@ -21,20 +31,47 @@ const Login = () => {
         setErrors(e.message);
       });
   };
+  function condition() {
+    if (user?.email == null) {
+      return (
+        <div>
+          <form onSubmit={(e) => handleForm(e)}>
+            <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" placeholder="Email" />
+            <input className="form-control" onChange={(e) => setPassword(e.target.value)} name="password" value={password} type="password" placeholder="Password" />
+            <br />
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+            <br></br>
+            <span>{error}</span>
+          </form>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <span>Sebagai: {user?.email}</span>
+          <form onSubmit={(e) => logout(e)}>
+            <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" placeholder="Email" />
+            <input className="form-control" onChange={(e) => setPassword(e.target.value)} name="password" value={password} type="password" placeholder="Password" />
+            <br />
+            <button type="submit" className="btn btn-danger">
+              Logout
+            </button>
+            <br></br>
+            <span>{error}</span>
+          </form>
+        </div>
+      );
+    }
+  }
 
   return (
     <div>
-      <h1>Login</h1>
-      <form onSubmit={(e) => handleForm(e)}>
-        <input className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" placeholder="Email" />
-
-        <input className="form-control" onChange={(e) => setPassword(e.target.value)} name="password" value={password} type="password" placeholder="Password" />
-        <br />
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-        <span>{error}</span>
-      </form>
+      <h1>
+        Login<br></br>
+      </h1>
+      {condition()}
     </div>
   );
 };
--- Repo Web Framework/Minggu 10/react-firebase/src/index.js
+++ Repo Web Framework/Minggu 10/react-firebase/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import routes from './routes';
 import Header from './Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import firebase from "firebase/compat/app";
 import firebaseConfig from './firebase.config';
 import './styles.css';
