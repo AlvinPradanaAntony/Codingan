cf50df1aa4378556b8aba454e2862eaba6ef3bf5
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-05-28 17:00:14 +0700

    Update

---
+++ Repo Web Framework/Minggu 10/react-firebase/src/firebase.config.js
@@ -0,0 +1,8 @@
+export default{
+  apiKey: "AIzaSyB9p-WbmeThhu7cjyR0bzXZDSamRGkx5CA",
+  authDomain: "reactfirebase-96f6b.firebaseapp.com",
+  projectId: "reactfirebase-96f6b",
+  storageBucket: "reactfirebase-96f6b.appspot.com",
+  messagingSenderId: "634488125983",
+  appId: "1:634488125983:web:0a990b607bb1ab234e732a",
+};
--- Repo Web Framework/Minggu 10/react-firebase/src/index.js
+++ Repo Web Framework/Minggu 10/react-firebase/src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+// import {Switch,BrowserRouter as Router, Route } from "react-router-dom";
+// import routes from "./routes";
+// import Header from "./Header";
+import Login from "./Login";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/compat/app";
+import firebaseConfig from "./firebase.config";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      Is logged in?{JSON.stringify(isLoggedIn)}
+      <div className="App">
+        <Login />
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
