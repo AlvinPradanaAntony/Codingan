fa6ed2c9ab50d351a5a55da23feeb99abfaef8c1
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-05 13:15:03 +0700

    Update

--- Repo Web Framework/Minggu 11/react_firebase/src/firebase/firebase.js
+++ Repo Web Framework/Minggu 11/react_firebase/src/firebase/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB9p-WbmeThhu7cjyR0bzXZDSamRGkx5CA",
