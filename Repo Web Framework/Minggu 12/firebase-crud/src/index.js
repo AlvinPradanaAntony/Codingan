eb80f135ead41db7e1357af7cb7c6908c6c7bc1c
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-07 15:38:17 +0700

    Update

--- Repo Web Framework/Minggu 12/firebase-crud/src/index.js
+++ Repo Web Framework/Minggu 12/firebase-crud/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Show from "./components/Show";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/edit/:id" component={Edit} />
+        <Route path="/create" component={Create} />
+        <Route path="/show/:id" component={Show} />
+      </div>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
