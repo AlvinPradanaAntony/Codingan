44a8da76af5280fe5490d60acb639215fed0c9c9
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-10 00:26:33 +0700

    Update

--- My Portofolio/Latian/coba2/firebase-react-crud-main/src/Actions.js
+++ My Portofolio/Latian/coba2/firebase-react-crud-main/src/Actions.js
@@ -1,49 +1,39 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase-config";
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+const usersCollectionRef = collection(db, "users");
 
 function App() {
   const [newName, setNewName] = useState("");
-  const [id, setNewId] = useState("");
   const [newAge, setNewAge] = useState(0);
-
+  const [id, setId] = useState("");
   const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
 
   const createUser = async () => {
     await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
   };
 
-  // const updateUser = async (id, age) => {
-  //   const userDoc = doc(db, "users", id);
-  //   const newFields = { age: age + 1 };
-  //   await updateDoc(userDoc, newFields);
-  // };
-  const updateUser = async () => {
-    const userDoc = doc(db, "users", id);
-    await updateDoc(userDoc, { name: newName, age: newAge });
+  const updateUser = async (id, name, age) => {
+    setId(id);
+    setNewName(name);
+    setNewAge(age);
   };
+
+  const editUser = async () => {
+    const docRef = doc(db, "users", id);
+    updateDoc(docRef, { name: newName, age: Number(newAge) })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error.message));
+  }
 
   const deleteUser = async (id) => {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
   };
 
-  const handleChange = (id, name, age) => {
-    setNewId(id);
-    setNewName(name);
-    setNewAge(age);
-  };
-
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     const data = await getDocs(usersCollectionRef);
-  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  //   };
-
-  //   getUsers();
-  // }, []);
   useEffect(() => {
     const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
       setUsers(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
@@ -55,63 +45,59 @@ function App() {
 
   return (
     <div className="App">
-      <form>
-        <input
-          placeholder="Name..."
-          onChange={(event) => {
-            setNewName(event.target.value);
-          }}
-        />
-        <input
-          type="number"
-          placeholder="Age..."
-          onChange={(event) => {
-            setNewAge(event.target.value);
-          }}
-        />
-        <button onClick={createUser}> Add User</button>
-      </form>
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(event) => {
+          setNewAge(event.target.value);
+        }}
+      />
+      <button onClick={createUser}> Create User</button>
       <br></br>
-      <form>
-        <input
+      <input
         value={id}
-          placeholder="Id..."
-          onChange={(event) => {
-            setNewId(event.target.value);
-          }}
-        />
-        <input
-        value={newName}
-          placeholder="Name..."
-          onChange={(event) => {
-            setNewName(event.target.value);
-          }}
-        />
-        <input
-        value={newAge}
-          type="number"
-          placeholder="Age..."
-          onChange={(event) => {
-            setNewAge(event.target.value);
-          }}
-        />
+        placeholder="Id..."
+        onChange={(event) => {
+          setId(event.target.value);
+        }}
+      />
+      <input
+      value={newName}
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+      value={newAge}
+        type="string"
+        placeholder="Status..."
+        onChange={(event) => {
+          setNewAge(event.target.value);
+        }}
+      />
 
-        <button onClick={updateUser}> edit User</button>
-      </form>
+      <button onClick={editUser}> Edit User</button>
       {users.map((user) => {
         return (
-          <div>
+          <div key={user.id}>
             {" "}
-            <p>Id : {user.id}</p>
+            <p>Id {user.id}</p>
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
             <button
               onClick={() => {
-                handleChange(user.id, user.name, user.age);
+                updateUser(user.id, user.name, user.age);
               }}
             >
               {" "}
-              Edit
+              Increase Age
             </button>
             <button
               onClick={() => {
