33eb424bee2886f278af3af535c1256b4815c094
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-11 20:43:27 +0700

    update

---
+++ My Portofolio/Latian/Auth/Coba4/firebase-auth-context-main/src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5TGVgdM5goEa3lbPVWZuhblObTe_iN0Q",
+  authDomain: "latiancrud-25e2c.firebaseapp.com",
+  databaseURL: "https://latiancrud-25e2c-default-rtdb.firebaseio.com",
+  projectId: "latiancrud-25e2c",
+  storageBucket: "latiancrud-25e2c.appspot.com",
+  messagingSenderId: "514987945998",
+  appId: "1:514987945998:web:b509931bb75eabdc744c5f",
+  measurementId: "G-9GWKE9QSQC",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
