9f90538721764ce6bd6faad91fa507d513316eaa
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-11 17:19:51 +0700

    Update

---
+++ My Portofolio/Latian/Auth/Coba2/React-Firebase-Auth-master/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyC5TGVgdM5goEa3lbPVWZuhblObTe_iN0Q",
+  authDomain: "latiancrud-25e2c.firebaseapp.com",
+  databaseURL: "https://latiancrud-25e2c-default-rtdb.firebaseio.com",
+  projectId: "latiancrud-25e2c",
+  storageBucket: "latiancrud-25e2c.appspot.com",
+  messagingSenderId: "514987945998",
+  appId: "1:514987945998:web:b509931bb75eabdc744c5f",
+  measurementId: "G-9GWKE9QSQC"
+})
+
+export const auth = app.auth()
+export default app
