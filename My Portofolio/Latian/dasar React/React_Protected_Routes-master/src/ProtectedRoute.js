f06475af051470455b56f63efeba690596c65006
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-11 20:34:44 +0700

    Update

--- My Portofolio/Latian/dasar React/React_Protected_Routes-master/src/ProtectedRoute.js
+++ My Portofolio/Latian/dasar React/React_Protected_Routes-master/src/ProtectedRoute.js
@@ -6,11 +6,7 @@ const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
     <Route
       {...rest}
       render={(props) => {
-        if (auth) return <Component {...props} />;
-        if (!auth)
-          return (
-            <Redirect to={{ path: "/", state: { from: props.location } }} />
-          );
+        return auth? <Component {...props} />:<Redirect to={{ path: "/", state: { from: props.location } }} />
       }}
     />
   );
