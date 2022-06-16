4ea4ae973899cbe1d8cfbe9226c855026bbdc4a3
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-16 19:22:49 +0700

    Update

--- My Portofolio/Latian/ChatApp/coba1 (best)/react-firebase-chat-master/src/firebase.js
+++ My Portofolio/Latian/ChatApp/coba1 (best)/react-firebase-chat-master/src/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   storageBucket: "latiancrud-25e2c.appspot.com",
   messagingSenderId: "514987945998",
   appId: "1:514987945998:web:b509931bb75eabdc744c5f",
-  measurementId: "G-9GWKE9QSQC",
+  measurementId: "G-9GWKE9QSQC"
 };
 
 // Initialize Firebase
