73db6d0f50627fa69df3a0821cff7b029f9cd329
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-06-22 11:34:07 +0700

    Sidebar

--- My Portofolio/Latian/Dashboard/sidebar/src/App.js
+++ My Portofolio/Latian/Dashboard/sidebar/src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SideMenu, { menuItems } from "./components/SideMenu";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Content = () => <h1>Content</h1>;
+const Courses = () => <h1>Content/Courses</h1>;
+const Videos = () => <h1>Content/Videos</h1>;
+const Design = () => <h1>Design</h1>;
+const Content2 = () => <h1>Content2</h1>;
+const Courses2 = () => <h1>Content/Courses 2</h1>;
+const Videos2 = () => <h1>Content/Videos 2</h1>;
+const Design2 = () => <h1>Design 2</h1>;
+const Design3 = () => <h1>Design 3</h1>;
+const Design4 = () => <h1>Design 4</h1>;
 
 function App() {
+  const [inactive, setInactive] = useState(false);
+  const [active, setAvtive] =useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <SideMenu
+          onCollapses={(inactive) => {
+            console.log("State class inactive",inactive);
+            setInactive(inactive);
+          }}
+        />
+
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+          {/* improvememt, not recorded in video, its just looping through menuItems
+          instead of hard coding all the routes */}
+          {/* {menuItems.map((menu, index) => (
+            <>
+              <Route key={menu.name} exact={menu.exact} path={menu.to}>
+                <h1>{menu.name}</h1>
+              </Route>
+              {menu.subMenus && menu.subMenus.length > 0
+                ? menu.subMenus.map((subMenu, i) => (
+                    <Route key={subMenu.name} path={subMenu.to}>
+                      <h1>{subMenu.name}</h1>
+                    </Route>
+                  ))
+                : null}
+            </>
+          ))} */}
+
+          <Switch>
+            <Route exact path={"/"}>
+              <Dashboard />
+            </Route>
+            <Route exact path={"/content"}>
+              <Content />
+            </Route>
+            <Route path={"/content/courses"}>
+              <Courses />
+            </Route>
+            <Route path={"/content/videos"}>
+              <Videos />
+            </Route>
+            <Route path={"/design"}>
+              <Design />
+            </Route>
+            <Route exact path={"/content-2"}>
+              <Content2 />
+            </Route>
+            <Route path={"/content-2/courses"}>
+              <Courses2 />
+            </Route>
+            <Route path={"/content-2/videos"}>
+              <Videos2 />
+            </Route>
+            <Route path={"/design-2"}>
+              <Design2 />
+            </Route>
+            <Route path={"/design-3"}>
+              <Design3 />
+            </Route>
+            <Route path={"/design-4"}>
+              <Design4 />
+            </Route>
+          </Switch>
+        </div>
+      </Router> 
     </div>
   );
 }
