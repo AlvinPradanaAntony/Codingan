37ea75f2716227434105b0b29c337ac4d882383e
Alvin Pradana Antony <quickgaming321@gmail.com>
2022-12-23 13:12:59 +0700

    Fix input

--- ID3/Assets/Private/js/script.js
+++ ID3/Assets/Private/js/script.js
@@ -1,9 +1,11 @@
+/*========== SIDEBAR ACTIVE/DEACTIVATE ==========*/
 $(document).ready(function () {
   $("#menu").click(function (e) {
     $("#sidebar").toggleClass("close");
   });
 });
 
+/*========== COLLAPSE/ACCORDION ==========*/
 $(document).ready(function () {
   $(".collapse").on("show.bs.collapse", function () {
     $(".collapse.show").each(function () {
@@ -13,16 +15,15 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
   // Tambahkan class ketika collapse di klik
-  $(".collapse").on("show.bs.collapse", function () {
-    $(this).parent().addClass("nav-item-active");
-  }).on("hide.bs.collapse", function () {
-    $(".nav-item").removeClass("nav-item-active");
-  });
+  $(".collapse")
+    .on("show.bs.collapse", function () {
+      $(this).parent().addClass("nav-item-active");
+    })
+    .on("hide.bs.collapse", function () {
+      $(".nav-item").removeClass("nav-item-active");
+    });
 });
-$(document).ready(function () {
-
-});
-
+$(document).ready(function () {});
 
 /*==================== DARK LIGHT THEME ====================*/
 $(document).ready(function () {
@@ -40,11 +41,11 @@ $(document).ready(function () {
   localStorage.setItem("dark-mode", darkMode);
 
   if (localStorage.getItem("dark-mode") == "dark") {
-    // if the above is 'dark' then apply .dark to the body
     $("body").addClass("dark-theme");
     $("#theme-button").removeClass("uil-moon");
     $("#theme-button").addClass("uil-sun");
     $("#logo_sidebar").attr("src", "Assets/Private/img/logoW.png");
+    $("#img_login").attr("src", "Assets/Private/img/auth-img-dark.png");
 
     if ($("#sidebar").hasClass("close")) {
       $("#logo_sidebar")
@@ -60,6 +61,11 @@ $(document).ready(function () {
           $("#logo_sidebar").attr("width", "135");
         })
         .fadeIn(150);
+      $("#img_login")
+        .fadeOut(100, function () {
+          $("#img_login").attr("src", "Assets/Private/img/auth-img-dark.png");
+        })
+        .fadeIn(100);
     }
   }
 
@@ -68,7 +74,7 @@ $(document).ready(function () {
       $("#theme-button").removeClass("uil-sun");
       $("#theme-button").addClass("uil-moon");
       $("body").removeClass("dark-theme");
-      
+
       if ($("#sidebar").hasClass("close")) {
         $("#logo_sidebar")
           .fadeOut(150, function () {
@@ -83,6 +89,11 @@ $(document).ready(function () {
             $("#logo_sidebar").attr("width", "135");
           })
           .fadeIn(150);
+        $("#img_login")
+          .fadeOut(100, function () {
+            $("#img_login").attr("src", "Assets/Private/img/auth-img.png");
+          })
+          .fadeIn(100);
       }
       // set stored value to 'light'
       localStorage.setItem("dark-mode", "light");
@@ -90,21 +101,27 @@ $(document).ready(function () {
       $("#theme-button").removeClass("uil-moon");
       $("#theme-button").addClass("uil-sun");
       $("body").addClass("dark-theme");
-      
+
       if ($("#sidebar").hasClass("close")) {
         $("#logo_sidebar")
           .fadeOut(150, function () {
             $("#logo_sidebar").attr("src", "Assets/Private/img/logoLtW.png");
             $("#logo_sidebar").attr("width", "40");
           })
-          .fadeIn(150);
+          .fadeIn(100);
       } else {
         $("#logo_sidebar")
           .fadeOut(150, function () {
+            $("#img_login").attr("src", "Assets/Private/img/auth-img-dark.png");
             $("#logo_sidebar").attr("src", "Assets/Private/img/logoW.png");
             $("#logo_sidebar").attr("width", "135");
           })
           .fadeIn(150);
+        $("#img_login")
+          .fadeOut(100, function () {
+            $("#img_login").attr("src", "Assets/Private/img/auth-img-dark.png");
+          })
+          .fadeIn(100);
       }
       // set stored value to 'dark'
       localStorage.setItem("dark-mode", "dark");
@@ -124,9 +141,11 @@ $(document).ready(function () {
           }
         } else {
           if ($("body").hasClass("dark-theme")) {
+            $("#img_login").attr("src", "Assets/Private/img/auth-img-dark.png");
             $("#logo_sidebar").attr("src", "Assets/Private/img/logoW.png");
             $("#logo_sidebar").attr("width", "135");
           } else {
+            $("#img_login").attr("src", "Assets/Private/img/auth-img.png");
             $("#logo_sidebar").attr("src", "Assets/Private/img/logo.png");
             $("#logo_sidebar").attr("width", "135");
           }
@@ -143,4 +162,36 @@ $(document).ready(function () {
     $("#date-part").html(momentNow.format("dddd").substring(0, 20) + ", " + momentNow.format("DD MMMM YYYY"));
     $("#time-part").html(momentNow.format("hh:mm:ss A"));
   }, 100);
+});
+
+/*========== SHOW/HIDE PASSWORD INPUT ==========*/
+$("#passInput").keyup(function () {
+  var inputs = $("#passInput").val();
+  $("#spanEye").fadeIn("slow");
+  if (inputs == "") {
+    $("#spanEye").fadeOut("slow");
+    $("#spanEye").removeClass("show");
+    $("#spanEye").addClass("hidden");
+  } else {
+    $("#spanEye").fadeIn("slow");
+    $("#spanEye").removeClass("hidden");
+    $("#spanEye").addClass("show");
+  }
+});
+$(".show-hide").click(function () {
+  $(this).toggleClass("uil-eye-slash uil-eye");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+    $("#iconShowHide").css("color", "#5886ef");
+  } else {
+    input.attr("type", "password");
+    $("#iconShowHide").css("color", "#d8d8d8");
+  }
+});
+
+/*========== GET CURRENT YEAR ==========*/
+var currentYear = new Date().getFullYear();
+$(document).ready(function () {
+  $("#year").text(new Date().getFullYear());
 });
