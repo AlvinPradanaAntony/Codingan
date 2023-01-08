9f28f50b7fe8918e74ec7da0bfe9b0c7884d5cfe
Alvin Pradana Antony <quickgaming321@gmail.com>
2023-01-09 06:09:38 +0700

    /

--- Grading Jamur/GUI_gradingJamur.m
+++ Grading Jamur/GUI_gradingJamur.m
@@ -22,7 +22,7 @@ function varargout = GUI_gradingJamur(varargin)
 
 % Edit the above text to modify the response to help GUI_gradingJamur
 
-% Last Modified by GUIDE v2.5 09-Jan-2023 04:32:43
+% Last Modified by GUIDE v2.5 09-Jan-2023 06:00:50
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -256,7 +256,7 @@ set(gca,'XTick',[]);
 set(gca,'YTick',[]);
 set(gca,'color','#28293d');
 set(handles.edit1, 'String', '');
-set(handles.edit3, 'String', '');
+set(handles.edit2, 'String', '-');
 set(handles.edit3, 'String', '');
 set(handles.edit4, 'String', '');
 set(handles.pushbutton9,'Visible', 'Off');
@@ -586,9 +586,89 @@ function pushbutton18_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in pushbutton13.
 function pushbutton13_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton13 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+Img = im2double(handles.img);
+Img_gray = handles.img_gray;
+
+%konversi citra Grayscale menjadi citra biner
+    bw = imbinarize(Img_gray);
+    %     figure, imshow(bw)
+
+    %melakukan operasi morfologi untuk menyempurnakan hasil segmentasi
+    %1. filling holes
+    bw = imfill(bw,'holes');
+    %figure, imshow(bw)
+    %2. area opening
+    bw = bwareaopen(bw,1000);
+    %figure, imshow(bw)
+
+    %melakukan ekstraksi ciri warna RGB
+    R = Img(:,:,1); %red
+    G = Img(:,:,2); %green
+    B = Img(:,:,3); %blue
+    R(~bw) = 0;
+    G(~bw) = 0;
+    B(~bw) = 0;
+    %RGB = cat(3,R,G,B);
+    %figure, imshow(RGB)
+
+    Red = sum(sum(R))/sum(sum(bw));
+    Green = sum(sum(G))/sum(sum(bw));
+    Blue = sum(sum(B))/sum(sum(bw));
+
+    %melakukan ekstraksi ciri warna HSV
+    HSV = rgb2hsv(Img);
+    %figure, imshow(HSV)
+
+    H = HSV(:,:,1); %hue
+    S = HSV(:,:,2); %saturation
+    V = HSV(:,:,3); %value
+    H(~bw) = 0;
+    S(~bw) = 0;
+    V(~bw) = 0;
+
+    Hue = sum(sum(H))/sum(sum(bw));
+    Saturation = sum(sum(S))/sum(sum(bw));
+    Value= sum(sum(V))/sum(sum(bw));
+
+    %melakukan ekstraksi ciri tekstur GLCM
+    Img_gray(~bw) = 0;
+    %membentuk matriks kookurensi
+    GLCM = graycomatrix(Img_gray,'Offset',[0 1; -1 1; -1 0; -1 -1]);
+    stats = graycoprops(GLCM,{'Contrast','Correlation','Energy','Homogeneity'});
+    Contrast = mean(stats.Contrast);
+    Correlation = mean(stats.Correlation);
+    Energy = mean(stats.Energy);
+    Homogeneity= mean(stats.Homogeneity);
+
+    %mengisi variabel ciri uji dengan ciri hasil ekstraksi
+    ciri_uji(1,1) = Red;
+    ciri_uji(1,2) = Green;
+    ciri_uji(1,3) = Blue;
+    ciri_uji(1,4) = Hue;
+    ciri_uji(1,5) = Saturation;
+    ciri_uji(1,6) = Value;
+    ciri_uji(1,7) = Contrast;
+    ciri_uji(1,8) = Correlation;
+    ciri_uji(1,9) = Energy;
+    ciri_uji(1,10) = Homogeneity;
+
+
+    %memanggil nilai bobot jaringan
+    load('Mdl.mat')
+
+    %membaca kelas keluaran
+    kelas_keluaran = predict(Mdl,ciri_uji);
+
+    %mengubah nilai keluaran menjadi kelas keluaran
+    switch kelas_keluaran
+        case 1
+            kelas_keluaran = 'Putih Kekuningan';
+        case 2
+            kelas_keluaran = 'Putih';
+    end
+
+    %tampilkan citra asli dan kelas keluaran hasil pengujian
+    set(handles.edit5,'String', kelas_keluaran);
 
 
 % --- Executes on button press in pushbutton14.
