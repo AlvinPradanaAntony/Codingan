7e679d27f4a0633d550efeb504c73c72a7653b38
AlvinPradanaAntony <quickgaming321@gmail.com>
2024-09-19 19:49:04 +0700

    Update

--- Repo Mobile/Flutter/minggu11/lib/main.dart
+++ Repo Mobile/Flutter/minggu11/lib/main.dart
@@ -4,7 +4,12 @@ import 'login_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyCRh66pcvbWNuTAGOUUp1dE4f2UMBh6m40',
+      appId: '1:155778658180:android:fd8bed98729cfd611ff7d1',
+      projectId: 'minggu11-60416',
+      messagingSenderId: '155778658180',
+    )
   );
   runApp(MyApp());
 }
