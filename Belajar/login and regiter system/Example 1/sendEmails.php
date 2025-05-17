2c4c4528ba42d03f354a22237ab9b62aeef641d2
AlvinPradanaAntony <quickgaming321@gmail.com>
2025-05-17 11:12:31 +0700

    maintenance

--- Belajar/login and regiter system/Example 1/sendEmails.php
+++ Belajar/login and regiter system/Example 1/sendEmails.php
@@ -2,17 +2,17 @@
 require_once 'vendor/autoload.php';
 
 // Create the Transport
-$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-    ->setUsername('SENDER_EMAIL')
-    ->setPassword('SENDER_PASSWORD');
+$transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
+  ->setUsername('dev.tourify@gmail.com')
+  ->setPassword('ntdg bvoh itoj hidp');
 
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
 
 function sendVerificationEmail($userEmail, $token)
 {
-    global $mailer;
-    $body = '<!DOCTYPE html>
+  global $mailer;
+  $body = '<!DOCTYPE html>
     <html lang="en">
 
     <head>
@@ -37,24 +37,24 @@ function sendVerificationEmail($userEmail, $token)
     <body>
       <div class="wrapper">
         <p>Thank you for signing up on our site. Please click on the link below to verify your account:.</p>
-        <a href="http://http://localhost/Belajar/login%20and%20regiter%20system/Example%201/verify_email.php?token=' . $token . '">Verify Email!</a>
+        <a href="http://localhost/Belajar/login%20and%20regiter%20system/Example%201/verify_email.php?token=' . $token . '">Verify Email!</a>
       </div>
     </body>
 
     </html>';
 
-    // Create a message
-    $message = (new Swift_Message('Verify your email'))
-        ->setFrom('SENDER_EMAIL')
-        ->setTo($userEmail)
-        ->setBody($body, 'text/html');
+  // Create a message
+  $message = (new Swift_Message('Verify your email'))
+    ->setFrom('dev.tourify@gmail.com')
+    ->setTo($userEmail)
+    ->setBody($body, 'text/html');
 
-    // Send the message
-    $result = $mailer->send($message);
+  // Send the message
+  $result = $mailer->send($message);
 
-    if ($result > 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if ($result > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
---
+++ Belajar/login and regiter system/Example 1/verify-user.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `verify-user`;
+
+USE `verify-user`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `token` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `verified` BOOLEAN DEFAULT FALSE,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
